5d16faa1078c0ad415d41c7cd07f6b143f9bb529
Travis Fernandes <travisfernandes2327@gmail.com>
2024-09-05 15:58:12 +0000

    csv_update

--- adam/python/obd/test4.py
+++ adam/python/obd/test4.py
@@ -7,17 +7,15 @@ import datetime
 import requests
 import serial
 
-obd_connector="/dev/ttyACM0"
-print(obd_connector)
-
-obd.logger.setLevel(obd.logging.DEBUG)
+# obd_connector="/dev/ttyACM0"
+obd_connector = '/dev/pts/2'
 connection = obd.Async(obd_connector)
 
 # a callback that prints every new value to the console
 def new_rpm(r):
-    print (r.value)
+    print(r.value)
 
-connection.watch(obd.commands.COOLANT_TEMP, callback=new_rpm)
+connection.watch(obd.commands.PIDS_9A, callback=new_rpm)
 connection.start()
 
 # the callback will now be fired upon receipt of new values
